cdba2cf3bd421379029b8521e8e32a58b44d5a93
RexVN <147121750+RexVN2005@users.noreply.github.com>
2025-04-24 10:08:39 +0700

    Add files via upload

---
+++ OrderProcessor_Old.cs
@@ -0,0 +1,9 @@
+public class OrderProcessor_Old
+{
+    public void ProcessOrder(Order order)
+    {
+        Console.WriteLine("Processing order using old system");
+        // Legacy code for processing orders
+        Console.WriteLine($"Order {order.OrderId} processed with old system");
+    }
+} 
