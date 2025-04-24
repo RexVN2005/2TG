2ab234bd259cdf60b969425cde06edba9fd73d44
Thi <hadoantructhi299@gmail.com>
2025-04-24 11:31:09 +0700

    Begin factory

--- OrderProcessor_Old.cs
+++
@@ -1,9 +0,0 @@
-public class OrderProcessor_Old
-{
-    public void ProcessOrder(Order order)
-    {
-        Console.WriteLine("Processing order using old system");
-        // Legacy code for processing orders
-        Console.WriteLine($"Order {order.OrderId} processed with old system");
-    }
-} 
